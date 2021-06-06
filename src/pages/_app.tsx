8dc0fd5237fd4094cf7d63e6ed702b4c2c6d7e2f
Vexx <marius@modlich.de>
2021-06-07 01:33:06 +0200

    LFG

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,8 @@
 import { Chakra } from '../Chakra';
-import { ChainId, DAppProvider } from '@usedapp/core';
 
 import { AppProps } from 'next/app';
 
 import Head from 'next/head';
-
-const config = {
-  supportedChains: [1, 56],
-  multicallAddresses: {
-    [ChainId.Mainnet]: '0xeefba1e63905ef1d7acba5a8513c70307c1ce441',
-    [56]: '0x949f41e8a6197f2a19854f813fd361bab9aa7d2d',
-  },
-};
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -24,9 +15,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           content='MoonBud - A deflationary charity cryptocurrency for all dogs in the world.'
         />
       </Head>
-      <DAppProvider config={config}>
-          <Component {...pageProps} />
-      </DAppProvider>
+       <Component {...pageProps} />
     </Chakra>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,12 @@ import { Fragment } from 'react';
 
 import { Box, Text } from '@chakra-ui/react';
 
-
-
-
 const Index = () => (
   <Fragment>
- <Box bg="black" w="full" h="full"><Text color="white">Big</Text> <Text color="white" >Fat</Text> <Text color="white" >L</Text> <Text color="red.500" >RUGPULLED</Text>
+    <Box bg='black' w='full' h='full'>
+      <Text color='white'>Big</Text> <Text color='white'>Fat</Text>{' '}
+      <Text color='white'>L</Text> <Text color='red.500'>RUGPULLED</Text>
+    </Box>
   </Fragment>
 );
 
